84b82fa2a3886b6fe248da4e36a822130966efef
akashsolanki1101 <akashsolanki2509@gmail.com>
2020-07-24 12:53:25 +0530

    Dashboard page basic setup is done.

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Button ,TextInput} from "react-native";
+import Colors from '../constants/color-palete'
+import * as DocumentPicker from 'expo-document-picker';
 
 export const DashboardScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Dashboard Screen</Text>
+      <Image source={require('../assets/logo.jpg')} style={styles.albumImage}/>
+      <Text>Publish on Mello</Text>
+      <Text>Upload your first audio</Text>
+      <Button 
+        title="Start Uploading"
+        onPress={()=>DocumentPicker.getDocumentAsync({type:'image/*'})}
+        color={Colors.primary}
+      />
     </View>
   );
 };
@@ -15,4 +24,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  albumImage:{
+    width: 300,
+    height: 200,
+    backgroundColor: Colors.logobackground,
+    borderWidth: 1,
+    marginVertical: 30,
+    // borderRadius: 50,
+  }
 });
