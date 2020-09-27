23be0dbadc936e2791587633d14468a57a12c460
SWAPNIL RAUT <swapnilraut@SWAPNILs-MacBook-Pro.local>
2020-09-27 23:11:03 +0530

    meta data mapping

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,328 +1,296 @@
-import React,{useState,useEffect,useCallback} from 'react'
+import React, {useState, useEffect, useCallback} from 'react';
 
-import {View, Text, StyleSheet, Image ,Alert,TouchableOpacity,Share} from 'react-native'
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Alert,
+  TouchableOpacity,
+  Share,
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import TrackPlayer from 'react-native-track-player';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 
-import {MusicProgressBar} from './AudioPlayerProgressBar'
-import {Loader} from "./Loader"
+import {MusicProgressBar} from './AudioPlayerProgressBar';
+import {Loader} from './Loader';
 
-export const AudioPlayer = ({track,setShowModal})=>{
-    const [trackPlayerLoaded,setTrackPlayerLoaded] = useState(false)
-    const [isPlaying,setIsPlaying] = useState(false)
-    const [playerStopped,setIsStopped] = useState(true)
-    const [showLoader,setShowLoader] = useState(false)
-    
-    const start =useCallback(async () => {
-        try{
-            await TrackPlayer.setupPlayer();
-            TrackPlayer.updateOptions({
-                stopWithApp: true,
-                capabilities: [
-                    TrackPlayer.CAPABILITY_PLAY,
-                    TrackPlayer.CAPABILITY_PAUSE,
-                    TrackPlayer.CAPABILITY_SEEK_TO,
-                    TrackPlayer.CAPABILITY_JUMP_FORWARD,
-                    TrackPlayer.CAPABILITY_JUMP_BACKWARD,
-                ],
-                compactCapabilities: [
-                    TrackPlayer.CAPABILITY_PLAY,
-                    TrackPlayer.CAPABILITY_PAUSE,
-                    TrackPlayer.CAPABILITY_JUMP_FORWARD,
-                    TrackPlayer.CAPABILITY_JUMP_BACKWARD,
-                ],
-                notificationCapabilities: [
-                    TrackPlayer.CAPABILITY_PLAY,
-                    TrackPlayer.CAPABILITY_PAUSE,
-                    TrackPlayer.CAPABILITY_SEEK_TO,
-                    TrackPlayer.CAPABILITY_JUMP_FORWARD,
-                    TrackPlayer.CAPABILITY_JUMP_BACKWARD,
-                ],
-                jumpInterval: 10
-            });
-            setTrackPlayerLoaded(true)
-        }catch(err){
-            Alert.alert(
-                'SET PLAYER',
-                `${err.message}`)
+export const AudioPlayer = ({track, setShowModal}) => {
+  const [trackPlayerLoaded, setTrackPlayerLoaded] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playerStopped, setIsStopped] = useState(true);
+  const [showLoader, setShowLoader] = useState(false);
+
+  const start = useCallback(async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      TrackPlayer.updateOptions({
+        stopWithApp: true,
+        capabilities: [
+          TrackPlayer.CAPABILITY_PLAY,
+          TrackPlayer.CAPABILITY_PAUSE,
+          TrackPlayer.CAPABILITY_SEEK_TO,
+          TrackPlayer.CAPABILITY_JUMP_FORWARD,
+          TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+        ],
+        compactCapabilities: [
+          TrackPlayer.CAPABILITY_PLAY,
+          TrackPlayer.CAPABILITY_PAUSE,
+          TrackPlayer.CAPABILITY_JUMP_FORWARD,
+          TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+        ],
+        notificationCapabilities: [
+          TrackPlayer.CAPABILITY_PLAY,
+          TrackPlayer.CAPABILITY_PAUSE,
+          TrackPlayer.CAPABILITY_SEEK_TO,
+          TrackPlayer.CAPABILITY_JUMP_FORWARD,
+          TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+        ],
+        jumpInterval: 10,
+      });
+      setTrackPlayerLoaded(true);
+    } catch (err) {
+      Alert.alert('SET PLAYER', `${err.message}`);
+    }
+
+    try {
+      await TrackPlayer.add(track);
+    } catch (err) {
+      Alert.alert('Add Track', `${err.message}`);
+    }
+
+    setIsPlaying(true);
+    await TrackPlayer.play();
+    setIsStopped(false);
+  }, [track]);
+
+  useEffect(() => {
+    start();
+  }, [start]);
+
+  const handleShowModel = () => {
+    setShowModal(false);
+    setIsPlaying(false);
+  };
+
+  const handleTrackPlay = async () => {
+    await TrackPlayer.play();
+    setIsPlaying(true);
+  };
+
+  const handleTrackPause = () => {
+    TrackPlayer.pause();
+    setIsPlaying(false);
+  };
+
+  const backwardHandler = async () => {
+    const currentPosition = await TrackPlayer.getPosition();
+    if (currentPosition <= 10) {
+      TrackPlayer.seekTo(0);
+    } else {
+      TrackPlayer.seekTo(currentPosition - 10);
+    }
+  };
+
+  const forwardHandler = async () => {
+    const currentPosition = await TrackPlayer.getPosition();
+    TrackPlayer.seekTo(currentPosition + 10);
+  };
+
+  // const nextTrackHandler = async ()=>{
+  //     try{
+  //         await TrackPlayer.skipToNext()
+  //         const currTrackId = await TrackPlayer.getCurrentTrack()
+  //         const currTrackObj = await TrackPlayer.getTrack(currTrackId)
+  //         setCurrentTrack(currTrackObj)
+  //     }
+  //     catch(err){
+  //         Alert.alert(`${err.message}`)
+  //     }
+  // }
+
+  // const previousTrackHandler = async ()=>{
+  //     try{
+  //         await TrackPlayer.skipToPrevious()
+  //         const currTrackId = await TrackPlayer.getCurrentTrack()
+  //         const currTrackObj = await TrackPlayer.getTrack(currTrackId)
+  //         setCurrentTrack(currTrackObj)
+  //     }
+  //     catch(err){
+  //         Alert.alert(`${err.message}`)
+  //     }
+  // }
+
+  const sliderHandler = (value: number) => {
+    TrackPlayer.seekTo(value);
+    // setIsPlaying(true)
+  };
+
+  TrackPlayer.addEventListener('remote-play', () => {
+    handleTrackPlay();
+  });
+
+  TrackPlayer.addEventListener('remote-pause', () => {
+    handleTrackPause();
+  });
+
+  const handleShareButtonClick = async () => {
+    setShowLoader(true);
+    try {
+      const link = await dynamicLinks().buildShortLink({
+        domainUriPrefix: 'https://melloeverywhere.page.link',
+        link: `http://creator.yourmello.com/query?docId=${track.id}`,
+        android: {
+          packageName: 'com.audiogram',
+        },
+        social: {
+          imageUrl: track.artwork,
+          descriptionText: 'Listen this wonderful song.',
+          title: track.title,
+        },
+      });
+
+      const result = await Share.share({
+        message: link,
+      });
+
+      setShowLoader(false);
+
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log(result.activityType);
+        } else {
+          console.log('shared');
         }
-
-        try{       
-            await TrackPlayer.add(track);
-        }catch(err){
-            Alert.alert(
-                'Add Track',
-                `${err.message}`
-                )
-        }
-
-        setIsPlaying(true)
-        await TrackPlayer.play();
-        setIsStopped(false)
-    },[track]);
-
-    useEffect(()=>{
-       start()
-    },[start])
-
-    const handleShowModel = ()=>{
-        setShowModal(false)
-        setIsPlaying(false)
+      } else if (result.action === Share.dismissedAction) {
+        console.log('not shared');
+      }
+    } catch (err) {
+      console.log(err);
     }
-
-    const handleTrackPlay = async ()=>{
-        await TrackPlayer.play()
-        setIsPlaying(true)
-    }
-
-    const handleTrackPause = ()=>{
-        TrackPlayer.pause()
-        setIsPlaying(false)
-    }
-
-    const backwardHandler = async()=>{
-        const currentPosition = await TrackPlayer.getPosition()
-        if(currentPosition<=10){
-            TrackPlayer.seekTo(0)
-        }else{
-            TrackPlayer.seekTo(currentPosition - 10)
-        }
-    }
-
-    const forwardHandler = async()=>{
-        const currentPosition = await TrackPlayer.getPosition()
-        TrackPlayer.seekTo(currentPosition + 10)
-    }
-
-    // const nextTrackHandler = async ()=>{
-    //     try{
-    //         await TrackPlayer.skipToNext()
-    //         const currTrackId = await TrackPlayer.getCurrentTrack()
-    //         const currTrackObj = await TrackPlayer.getTrack(currTrackId)
-    //         setCurrentTrack(currTrackObj)
-    //     }
-    //     catch(err){
-    //         Alert.alert(`${err.message}`)
-    //     }
-    // }
-
-    // const previousTrackHandler = async ()=>{
-    //     try{
-    //         await TrackPlayer.skipToPrevious()
-    //         const currTrackId = await TrackPlayer.getCurrentTrack()
-    //         const currTrackObj = await TrackPlayer.getTrack(currTrackId)
-    //         setCurrentTrack(currTrackObj)
-    //     }
-    //     catch(err){
-    //         Alert.alert(`${err.message}`)
-    //     }
-    // }
-
-    const sliderHandler = (value:number)=>{
-        TrackPlayer.seekTo(value)
-        // setIsPlaying(true)
-    }
-
-    TrackPlayer.addEventListener('remote-play', () => {
-        handleTrackPlay()
-    });
-    
-    TrackPlayer.addEventListener('remote-pause', () => {
-    handleTrackPause()
-    });
-
-    const handleShareButtonClick = async ()=>{
-        setShowLoader(true)
-        try{
-            const link = await dynamicLinks().buildShortLink({
-                domainUriPrefix: 'https://melloeverywhere.page.link',
-                link: `http://creator.yourmello.com/query?docId=${track.id}`,
-                android:{
-                    packageName:'com.audiogram'
-                },
-                social:{
-                    imageUrl:track.artwork,
-                    descriptionText:"Listen this wonderful song.",
-                    title:track.title
-                },
-            });            
-            
-            const result = await Share.share({
-                message : link,
-            })
-            
-            setShowLoader(false)
-
-            if(result.action === Share.sharedAction){
-                if(result.activityType){
-                    console.log(result.activityType);
-                }
-                else{
-                    console.log('shared');
-                }
-            }else if(result.action===Share.dismissedAction){
-                console.log("not shared");
-            }
-        }catch(err){
-            console.log(err);
-        }
-    }
-    
-
-    return(
-        <View style={styles.screen}> 
-            <View style={styles.header}>
-                <TouchableOpacity
-                    activeOpacity={.5}
-                    onPress={handleShowModel}
-                >
-                    <AntDesign 
-                        name = "down"
-                        size = {24}
-                        color = "black"
-                    />
-                </TouchableOpacity>
-                <Text style={styles.headerText}>PLAYING NOW</Text>
-                <TouchableOpacity
-                    activeOpacity={.5}
-                    onPress={handleShareButtonClick}
-                >
-                    <FontAwesome 
-                        name="share-alt" 
-                        size={24} 
-                        color="black" />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.controlSection}>
-                <View style={styles.trackCoverImageContainer}>
-                    <Image
-                        source={require('../assets/logo1.jpg')}
-                        style={styles.trackCoverImage}
-                    ></Image>
-                </View>
-                <View style={styles.trackDetailsContainer}>
-                <Text style={styles.trackTitle}>{track.title}</Text>
-                    <Text style={styles.trackArtist}>{track.artist}</Text>
-                </View>
-                <MusicProgressBar 
-                    sliderHandler = {sliderHandler}
-                    setIsPlaying = {setIsPlaying}
-                    setIsStopped = {setIsStopped}
-                />
-                <View style={styles.controlButton}>
-                    <TouchableOpacity
-                        activeOpacity={.5}
-                        onPress={backwardHandler}
-                    >
-                        <MaterialIcons 
-                            name="replay-10" 
-                            size={30} 
-                            color="black"
-                        />
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        activeOpacity={.5}
-                        onPress={isPlaying ? handleTrackPause : handleTrackPlay}
-                    >
-                        <View style={styles.playButtonContainer}>
-                            <AntDesign 
-                                name={isPlaying ? "pausecircleo" : "playcircleo"} 
-                                size={38} 
-                                color="black" 
-                            />
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        activeOpacity={.5}
-                        onPress={forwardHandler}
-                    >
-                        <MaterialIcons 
-                            name="forward-10" 
-                            size={30} 
-                            color="black" 
-                        />
-                    </TouchableOpacity>
-                </View>
-            </View>
-            {
-                showLoader&&<Loader/>
-            }
+  };
+  const trackImageUri = {uri: track.artwork};
+  return (
+    <View style={styles.screen}>
+      <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0.5} onPress={handleShowModel}>
+          <AntDesign name="down" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>PLAYING NOW</Text>
+        <TouchableOpacity activeOpacity={0.5} onPress={handleShareButtonClick}>
+          <FontAwesome name="share-alt" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.controlSection}>
+        <View style={styles.trackCoverImageContainer}>
+          <Image source={trackImageUri} style={styles.trackCoverImage} />
         </View>
-    )
-}
+        <View style={styles.trackDetailsContainer}>
+          <Text style={styles.trackTitle}>{track.title}</Text>
+          <Text style={styles.trackArtist}>{track.artist}</Text>
+        </View>
+        <MusicProgressBar
+          sliderHandler={sliderHandler}
+          setIsPlaying={setIsPlaying}
+          setIsStopped={setIsStopped}
+        />
+        <View style={styles.controlButton}>
+          <TouchableOpacity activeOpacity={0.5} onPress={backwardHandler}>
+            <MaterialIcons name="replay-10" size={30} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={isPlaying ? handleTrackPause : handleTrackPlay}>
+            <View style={styles.playButtonContainer}>
+              <AntDesign
+                name={isPlaying ? 'pausecircleo' : 'playcircleo'}
+                size={38}
+                color="black"
+              />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5} onPress={forwardHandler}>
+            <MaterialIcons name="forward-10" size={30} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {showLoader && <Loader />}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen : {
-        flex : 1,
-        backgroundColor : 'white',
-        display : 'flex',
-        justifyContent : 'space-between',
-        alignItems : 'center',
-        paddingVertical : 30,
-    },
-    header:{
-        width : '90%',
-        display : 'flex',
-        flexDirection : 'row',
-        justifyContent : 'space-between',
-        alignItems : 'center',
-    },
-    headerText:{
-        fontSize : 20,
-        fontWeight :'700',
-        color : 'black'
-    },
-    controlSection : {
-        width : '90%',
-        height : '85%',
-        // borderWidth : 1,
-        display : 'flex',
-        justifyContent : 'space-around',
-        alignItems : 'center'
-    },
-    trackCoverImageContainer:{
-        width : '70%',
-        height : '50%',
-        borderRadius : 20,
-        overflow : 'hidden',
-        elevation : 5,
-        backgroundColor:'white',
-    },
-    trackCoverImage:{
-        width : '100%',
-        height : '100%',
-        // transform : 
-    },
-    controlButton:{
-        width : '70%',
-        // borderWidth : 1,
-        display : 'flex',
-        flexDirection : 'row',
-        justifyContent : 'space-around',
-        alignItems : 'center'
-    },
-    playButtonContainer:{
-        display : 'flex',
-        justifyContent : 'center',
-        alignItems : 'center'
-    },
-    trackDetailsContainer:{
-        width : '100%',
-        // borderWidth : 1,
-        display :'flex',
-        justifyContent : 'center',
-        alignItems : 'center'
-    },
-    trackTitle:{
-        fontSize : 25,
-        fontWeight : '700'
-    },
-    trackArtist:{
-        fontSize : 18,
-        fontWeight : '400',
-        color : '#bbb'
-    },
-})
+  screen: {
+    flex: 1,
+    backgroundColor: 'white',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 30,
+  },
+  header: {
+    width: '90%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: 'black',
+  },
+  controlSection: {
+    width: '90%',
+    height: '85%',
+    // borderWidth : 1,
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  trackCoverImageContainer: {
+    width: '70%',
+    height: '50%',
+    borderRadius: 20,
+    overflow: 'hidden',
+    elevation: 5,
+    backgroundColor: 'white',
+  },
+  trackCoverImage: {
+    width: '100%',
+    height: '100%',
+    // transform :
+  },
+  controlButton: {
+    width: '70%',
+    // borderWidth : 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  playButtonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  trackDetailsContainer: {
+    width: '100%',
+    // borderWidth : 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  trackTitle: {
+    fontSize: 25,
+    fontWeight: '700',
+  },
+  trackArtist: {
+    fontSize: 18,
+    fontWeight: '400',
+    color: '#bbb',
+  },
+});
--- screens/HomePage.tsx
+++ screens/HomePage.tsx
@@ -1,149 +1,141 @@
-import React,{useState,useEffect,useCallback} from "react";
+import React, {useState, useEffect, useCallback} from 'react';
 
 import firestore from '@react-native-firebase/firestore';
-import {View ,StyleSheet,Alert, ActivityIndicator,RefreshControl} from "react-native";
-import {useDispatch,useSelector} from "react-redux"
+import {
+  View,
+  StyleSheet,
+  Alert,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 
-import { Catagory } from "../components/catagories/Catagory";
-import {setAudios} from  "../store/actions/audiosActions"
-import {setCategories} from "../store/actions/categoriesActions"
-import {setLanguages} from "../store/actions/languagesActions"
-import Colors from "../constants/color-palete"
-import {AudiosList} from "../components/AudiosList"
+import {Catagory} from '../components/catagories/Catagory';
+import {setAudios} from '../store/actions/audiosActions';
+import {setCategories} from '../store/actions/categoriesActions';
+import {setLanguages} from '../store/actions/languagesActions';
+import Colors from '../constants/color-palete';
+import {AudiosList} from '../components/AudiosList';
 
 export const HomePage = () => {
-  const [showSpinner,setShowSpinner] = useState(false)
-  const [isRefreshing,setIsRefreshing] = useState(false)
-  const categories = useSelector(state => state.categories.categories)
-  const audios = useSelector(state => state.audios.audios)
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const categories = useSelector((state) => state.categories.categories);
+  const audios = useSelector((state) => state.audios.audios);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const fetchData = useCallback(async()=>{
-    setIsRefreshing(true)
-    
-    const data:{}[] = []
-    const languages:{}[] = []
-    const categories:{}[] = []
-    const collRef = firestore().collection(`mello/data/audios`)
-    const collRef1 = firestore().collection(`mello/data/languages`).orderBy("name","asc")
-    const collRef2 = firestore().collection(`mello/data/categories`).orderBy("name","asc")
+  const fetchData = useCallback(async () => {
+    setIsRefreshing(true);
 
-    try{
-      const snapShot = await collRef.get()
-      const snapShot1 = await collRef1.get()
-      const snapShot2 = await collRef2.get()
-      snapShot.forEach(snap=>{
-        if(snap.exists){
+    const data: {}[] = [];
+    const languages: {}[] = [];
+    const categories: {}[] = [];
+    const collRef = firestore().collection(`mello/data/audios`);
+    const collRef1 = firestore()
+      .collection(`mello/data/languages`)
+      .orderBy('name', 'asc');
+    const collRef2 = firestore()
+      .collection(`mello/data/categories`)
+      .orderBy('name', 'asc');
+
+    try {
+      const snapShot = await collRef.get();
+      const snapShot1 = await collRef1.get();
+      const snapShot2 = await collRef2.get();
+      snapShot.forEach((snap) => {
+        if (snap.exists) {
           data.push({
-              key : snap.id,
-              id : snap.id,
-              url : snap.data().audiourl,
-              title : snap.data().name,
-              artist : snap.data().author.name,
-              artwork : snap.data().audiocoverurl,
-              duration : snap.data().duration,
-              category : snap.data().category,
-              status : snap.data().status,
-              userAvatar : snap.data().author.avatar,
-              language : snap.data().language
-            })
+            key: snap.id,
+            id: snap.id,
+            url: snap.data().audiourl,
+            title: snap.data().title,
+            artist: snap.data().author.name,
+            artwork: snap.data().audiocoverurl,
+            duration: snap.data().duration,
+            category: snap.data().category,
+            status: snap.data().status,
+            userAvatar: snap.data().author.avatar,
+            language: snap.data().language,
+            site: snap.data().site,
+            description: snap.data().description,
+          });
+        } else {
+          Alert.alert('Error!!!', 'Document not found');
         }
-        else{
-          Alert.alert(
-            'Error!!!',
-            "Document not found"
-          )
-        }
-      })
+      });
 
-      snapShot1.forEach(snap=>{
-        if(snap.exists){
+      snapShot1.forEach((snap) => {
+        if (snap.exists) {
           languages.push({
-            id : snap.id,
-            name : snap.data().name,
-            dbname : 'not-assigned'
-          })
+            id: snap.id,
+            name: snap.data().name,
+            dbname: 'not-assigned',
+          });
+        } else {
+          Alert.alert('Error!!!', 'Document not found');
         }
-        else{
-          Alert.alert(
-            'Error!!!',
-            "Document not found"
-          )
-        }
-        })
+      });
 
-      snapShot2.forEach(snap=>{
-        if(snap.exists){
+      snapShot2.forEach((snap) => {
+        if (snap.exists) {
           categories.push({
-            id : snap.id,
-            name : snap.data().name,
-            dbname : snap.data().dbname
-          })
+            id: snap.id,
+            name: snap.data().name,
+            dbname: snap.data().dbname,
+          });
+        } else {
+          Alert.alert('Error!!!', 'Document not found');
         }
-        else{
-          Alert.alert(
-            'Error!!!',
-            "Document not found"
-          )
-        }    
-      })
-
-      }
-    catch(err){
-      console.error(err)
+      });
+    } catch (err) {
+      console.error(err);
     }
-      dispatch(setAudios(data))
-      dispatch(setCategories(categories))
-      dispatch(setLanguages(languages))
-      setIsRefreshing(false)
-    }
-  ,[dispatch])
+    dispatch(setAudios(data));
+    dispatch(setCategories(categories));
+    dispatch(setLanguages(languages));
+    setIsRefreshing(false);
+  }, [dispatch]);
 
   // const handleDynamicLink = (link) => {
   //   console.log("Homepage",link)
   // };
 
-  useEffect(() =>{
-    setShowSpinner(true)
-    fetchData().then(function(){
-      setShowSpinner(false)
-    })
+  useEffect(() => {
+    setShowSpinner(true);
+    fetchData().then(function () {
+      setShowSpinner(false);
+    });
 
     // const unsubscribe = dynamicLinks().onLink(handleDynamicLink);
     // return () => unsubscribe();
+  }, [fetchData]);
 
-  },[fetchData])
-
-  if(showSpinner){
-    return(
+  if (showSpinner) {
+    return (
       <View style={styles.loaderContainer}>
-        <ActivityIndicator color={Colors.menu} size={50}/>
+        <ActivityIndicator color={Colors.menu} size={50} />
       </View>
-    )
+    );
   }
 
-  return (
-    <AudiosList 
-      data={audios}
-    />
-  );
+  return <AudiosList data={audios} />;
 };
 
 const styles = StyleSheet.create({
-  scrollViewContainer:{
-    flexGrow : 1,
-    backgroundColor : 'white',
+  scrollViewContainer: {
+    flexGrow: 1,
+    backgroundColor: 'white',
   },
-  container:{
-    flex : 1,
+  container: {
+    flex: 1,
   },
-  loaderContainer :{
-    flex:1,
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:'white'
-  }
+  loaderContainer: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
 });
